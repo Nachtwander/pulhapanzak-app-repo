305510a99de33826e05192a3f6afe55bf70e494a
Nachtwander <131110427+Nachtwander@users.noreply.github.com>
2024-08-19 11:24:27 -0600

    update

--- pulhapanzak-app/src/main.ts
+++ pulhapanzak-app/src/main.ts
@@ -1,6 +1,14 @@
 import { bootstrapApplication } from '@angular/platform-browser';
-import { RouteReuseStrategy, provideRouter, withPreloading, PreloadAllModules } from '@angular/router';
-import { IonicRouteStrategy, provideIonicAngular } from '@ionic/angular/standalone';
+import {
+  RouteReuseStrategy,
+  provideRouter,
+  withPreloading,
+  PreloadAllModules,
+} from '@angular/router';
+import {
+  IonicRouteStrategy,
+  provideIonicAngular,
+} from '@ionic/angular/standalone';
 
 import { routes } from './app/app.routes';
 import { AppComponent } from './app/app.component';
@@ -14,6 +22,21 @@ bootstrapApplication(AppComponent, {
   providers: [
     { provide: RouteReuseStrategy, useClass: IonicRouteStrategy },
     provideIonicAngular(),
-    provideRouter(routes, withPreloading(PreloadAllModules)), provideFirebaseApp(() => initializeApp({"projectId":"pulhapanzak-app","appId":"1:435698089790:web:cd3b657b133185bcae766f","storageBucket":"pulhapanzak-app.appspot.com","locationId":"us-central","apiKey":"AIzaSyCrESF2jL38EM-fuGRZggl1rFS0AWrs7pk","authDomain":"pulhapanzak-app.firebaseapp.com","messagingSenderId":"435698089790","measurementId":"G-ZJ5S0SXRNZ"})), provideAuth(() => getAuth()), provideFirestore(() => getFirestore()), provideMessaging(() => getMessaging()), provideStorage(() => getStorage()),
+    provideRouter(routes, withPreloading(PreloadAllModules)),
+    provideFirebaseApp(() =>
+      initializeApp({
+        projectId: 'pulhapanzak-app',
+        appId: '1:435698089790:web:cd3b657b133185bcae766f',
+        storageBucket: 'pulhapanzak-app.appspot.com',
+        apiKey: 'AIzaSyCrESF2jL38EM-fuGRZggl1rFS0AWrs7pk',
+        authDomain: 'pulhapanzak-app.firebaseapp.com',
+        messagingSenderId: '435698089790',
+        measurementId: 'G-ZJ5S0SXRNZ',
+      })
+    ),
+    provideAuth(() => getAuth()),
+    provideFirestore(() => getFirestore()),
+    provideMessaging(() => getMessaging()),
+    provideStorage(() => getStorage()),
   ],
 });
