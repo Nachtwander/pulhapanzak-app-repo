05fe6fc2ec1f8034cfeee2fc340167258a6d5c2a
Nachtwander <131110427+Nachtwander@users.noreply.github.com>
2024-08-19 11:17:43 -0600

    agregando firebase sdk al proyecto de ionic

--- pulhapanzak-app/src/main.ts
+++ pulhapanzak-app/src/main.ts
@@ -4,11 +4,16 @@ import { IonicRouteStrategy, provideIonicAngular } from '@ionic/angular/standalo
 
 import { routes } from './app/app.routes';
 import { AppComponent } from './app/app.component';
+import { initializeApp, provideFirebaseApp } from '@angular/fire/app';
+import { getAuth, provideAuth } from '@angular/fire/auth';
+import { getFirestore, provideFirestore } from '@angular/fire/firestore';
+import { getMessaging, provideMessaging } from '@angular/fire/messaging';
+import { getStorage, provideStorage } from '@angular/fire/storage';
 
 bootstrapApplication(AppComponent, {
   providers: [
     { provide: RouteReuseStrategy, useClass: IonicRouteStrategy },
     provideIonicAngular(),
-    provideRouter(routes, withPreloading(PreloadAllModules)),
+    provideRouter(routes, withPreloading(PreloadAllModules)), provideFirebaseApp(() => initializeApp({"projectId":"pulhapanzak-app","appId":"1:435698089790:web:cd3b657b133185bcae766f","storageBucket":"pulhapanzak-app.appspot.com","locationId":"us-central","apiKey":"AIzaSyCrESF2jL38EM-fuGRZggl1rFS0AWrs7pk","authDomain":"pulhapanzak-app.firebaseapp.com","messagingSenderId":"435698089790","measurementId":"G-ZJ5S0SXRNZ"})), provideAuth(() => getAuth()), provideFirestore(() => getFirestore()), provideMessaging(() => getMessaging()), provideStorage(() => getStorage()),
   ],
 });
